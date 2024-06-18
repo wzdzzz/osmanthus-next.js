e275a67aa0333dec6776750134c8bff6416cdb9d
this_xx <new1161437123@gmail.com>
2024-06-18 10:20:41 +0800

    feat: 修复组件ts错误

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { DashIcon } from "@radix-ui/react-icons"
-import { OTPInput, OTPInputContext } from "input-otp"
+import { OTPInput, OTPInputContext, SlotProps } from "input-otp"
 
 import { cn } from "@/lib/utils"
 
@@ -35,7 +35,9 @@ const InputOTPSlot = React.forwardRef<
   React.ComponentPropsWithoutRef<"div"> & { index: number }
 >(({ index, className, ...props }, ref) => {
   const inputOTPContext = React.useContext(OTPInputContext)
-  const { char, hasFakeCaret, isActive } = inputOTPContext.slots[index]
+  const { char, hasFakeCaret, isActive } = inputOTPContext.slots[
+    index
+  ] as SlotProps
 
   return (
     <div
