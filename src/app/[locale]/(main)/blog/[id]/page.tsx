29b38ec026b56d43985175ebe11ba3128fbae104
wzdzzz <new1161437123@gmail.com>
2024-09-03 14:47:57 +0800

    feat: 删除blog页面

--- src/app/[locale]/(main)/blog/[id]/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function Page({ params }: { params: { id: string } }) {
-  const { id } = params
-  return <>{id}</>
-}
--- src/app/[locale]/(main)/blog/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BlockEditor } from "@/components/block-editor"
-
-export default function Page() {
-  return (
-    <div>
-      <BlockEditor />
-    </div>
-  )
-}
